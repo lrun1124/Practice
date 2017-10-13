841715d12ee5e633fcfb2a99203692e9170b4521
lrun <lrun1124@126.com>
2017-10-13 23:47:53 +0800

    Closure sample code

---
+++ Closure Problem.js
@@ -0,0 +1,26 @@
+//code 1　　
+   var name = "The Window";
+　　var object = {
+　　　　name : "My Object",
+　　　　getNameFunc : function(){
+　　　　　　return function(){
+　　　　　　　　return this.name;
+　　　　　　};
+　　　　}
+　　};
+　　alert(object.getNameFunc()());
+
+//code 2
+   var name = "The Window";
+　　var object = {
+　　　　name : "My Object",
+　　　　getNameFunc : function(){
+　　　　　　var that = this;
+　　　　　　return function(){
+　　　　　　　　return that.name;
+　　　　　　};
+　　　　}
+　　};
+　　alert(object.getNameFunc()());
+
+对于code1 匿名函数是闭包，访问时，沿着作用域链寻找this到window对象，code2因为getNameFunc定义that，作用域链找到这里就终止了
